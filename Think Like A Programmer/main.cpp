0e9ec591e6232c3dff88299bdbd7fa535aa54e8c
Sander <sander.schrieken@mac.com>
2018-06-12 14:52:40 +0200

    Initial Commit

---
+++ Think Like A Programmer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Think Like A Programmer
+//
+//  Created by Sander on 12/06/2018.
+//  Copyright © 2018 Sander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
